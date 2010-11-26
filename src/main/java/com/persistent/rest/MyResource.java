9c8522805314130c1af443c7c76f337bbab33937
Derrick J. Wippler <thrawn01@gmail.com>
2010-11-26 13:03:15 -0600

    Now Querying the HSQL Database in the REST Call

--- src/main/java/com/persistent/rest/MyResource.java
+++ src/main/java/com/persistent/rest/MyResource.java
@@ -11,11 +11,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import com.persistent.entity.Person;
 
-
-
-// The Java class will be hosted at the URI path "/myresource"
 @Path("/myresource")
 @Component
 public class MyResource {
@@ -23,21 +21,22 @@ public class MyResource {
 	@Autowired
 	SessionFactory sessionFactory;
 	
-	// The Java method will process HTTP GET requests
 	@GET
-	// The Java method will produce content identified by the MIME Media
-	// type "text/plain"
+    @Transactional
 	@Produces("text/plain")
+    @SuppressWarnings("unchecked")
 	public String getIt() {
-        //final Session session = sessionFactory.getCurrentSession();
-        //final Criteria criteria = session.createCriteria(Person.class);
-		//StringBuffer buffer = new StringBuffer();
-		//for (Person person2 : persons) {
-		//		buffer.append(person2.getName()).append(":").append(person2.getAge())
-		//			.append("\n");
-		//}
-		//return criteria.list();
-		return "Hi there: ";
+		// Query
+        final Session session = sessionFactory.getCurrentSession();
+        final Criteria criteria = session.createCriteria(Person.class);
+        
+        // Construct Result
+		StringBuffer buffer = new StringBuffer();
+		for(Person person2 : (List<Person>)criteria.list()) {
+			buffer.append(person2.getName()).append(":").append(person2.getAge()).append("\n");
+		}
+		// Return Result
+		return buffer.toString();
 	}
 }
 
